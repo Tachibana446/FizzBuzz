2528680750b53d75680640eafe4a5fad7f734a86
Tachibana446 <stinger33z@gmail.com>
2017-04-01 17:14:23 +0900

    [update]scalaをパターンマッチでコードを短く

--- fizzbuzz.scala
+++ fizzbuzz.scala
@@ -1,26 +1,12 @@
 object Main extends App {
 
-	def fizz(tpl:(Int,String)) =
-	{
-		val x = tpl._1
-		val y = tpl._2
-		val str = if(x % 3 == 0) y + "Fizz" else y
-		(x, str)
-	}
+  for(i <- 1 to 30){
+    (i % 3, i % 5) match{
+      case (0,0) => println("FizzBuzz")
+      case (0,_) => println("Fizz")
+      case (_,0) => println("Buzz")
+      case _ => println(i)
+    }
+  }
 
-	def buzz(tpl: (Int,String)) =
-	{
-		val x = tpl._1
-		val y = tpl._2
-		val str = if(x % 5 == 0) y + "Buzz" else y
-		(x, str)
-	}
-
-	def put(tpl: (Int,String)) =
-	{
-		val out = if(tpl._2 == "") tpl._1.toString else tpl._2
-		println(out)
-	}
-
-	(1 to 30).map(x => (x,"")).map(fizz).map(buzz).foreach(put)
 }
