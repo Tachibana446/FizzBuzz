362b48f734d6b285b8adfb03a612b9de4ca70b50
Tachibana446 <stinger33z@gmail.com>
2017-04-03 16:31:23 +0900

    [update]再帰で関数型言語っぽさをだした

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,5 +1,6 @@
-1..30 |> Enum.map(
-fn x ->
+defmodule FizzBuzz do
+
+defp calc x do
   case {rem(x, 3),rem(x, 5)} do
     {0,0} -> "FizzBuzz"
     {0,_} -> "Fizz"
@@ -7,4 +8,19 @@ fn x ->
     _ -> "#{x}"
   end
 end
-) |> Enum.map(fn x -> IO.puts x end)
+
+defp print(n, limit) when n >= limit do
+  IO.puts calc(n)
+end
+
+defp print n, limit do
+  IO.puts calc(n)
+  print(n + 1, limit)
+end
+
+def run limit do
+  print 1, limit
+end
+end
+
+FizzBuzz.run(30)
